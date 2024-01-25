6dcea6f9cc02ab06faa92dc914d69f8bc7109592
Netology_n <an.zaickova.rr@yandex.ru>
2024-01-25 15:57:31 +0300

    Initial commit

---
+++ iter.py
@@ -0,0 +1,47 @@
+class FlatIterator:
+
+    def __init__(self, list_of_list):
+        self.list_of_list = list_of_list
+
+    def __iter__(self):
+        self.res_list = []
+        self.list_of_l1 = iter(self.list_of_list)
+        self.cursor = -1
+        return self
+
+    def __next__(self):
+        self.cursor += 1
+        if len(self.res_list) == self.cursor:
+            try:
+                self.cursor = 0
+                self.res_list = next(self.list_of_l1)
+            except StopIteration:
+                raise StopIteration
+        return self.res_list[self.cursor]
+
+
+list_flat = FlatIterator([[1, 2, 3], [4, 5, 6], [7]])
+for list_flat_item in list_flat:
+    print(list_flat_item)
+
+
+def test_1():
+
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            FlatIterator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+
+if __name__ == '__main__':
+    test_1()
